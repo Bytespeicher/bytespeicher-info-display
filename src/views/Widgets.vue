ee9ed0e205027e4a7a58f7806a76607bbd81a33b
Max Panzer <max-panzer@wiaazo.de>
2020-02-26 22:46:01 +0100

    Add app-header with back-button to widgets-view

--- src/views/Widgets.vue
+++ src/views/Widgets.vue
@@ -1,7 +1,22 @@
 <template>
-    <v-container fluid>
-        Widgets
-    </v-container>
+    <div>
+        <v-app-bar
+            color="blue-grey darken-2"
+            dark
+        >
+            <v-btn
+                text icon
+                color="white" to="/"
+            >
+                <v-icon>mdi-arrow-left</v-icon>
+            </v-btn>
+
+            <v-toolbar-title class="pl-2">{{ $t('widgets.title') }}</v-toolbar-title>
+        </v-app-bar>
+        <v-container fluid>
+
+        </v-container>
+    </div>
 </template>
 
 <script>
