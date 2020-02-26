788904f401c2ad47308f2106a1649b9994cd9d34
Max Panzer <max-panzer@wiaazo.de>
2020-02-26 19:59:34 +0100

    Add required plugins and modify eslintrc

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    transpileDependencies: [
+        'vuetify'
+    ],
+
+    pluginOptions: {
+        i18n: {
+            locale: 'de',
+            fallbackLocale: 'de',
+            localeDir: 'locales',
+            enableInSFC: false
+        }
+    }
+};
